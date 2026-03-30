0439646803d30f9f888f4aca6fa56866ca6078e8
krovw <marcspector486@gmail.com>
2026-03-30 12:38:56 -0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", color: "white" }}>
+      <h1>Meu site</h1>
+      <p>Backend e frontend próprios em construção.</p>
+    </main>
+  );
+}
